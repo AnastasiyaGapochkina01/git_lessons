09cbfa3bdd3b3c58a976ef79bac79f6862439d79
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2024-07-21 09:29:12 +0300

    Update rot13.php

--- rot13.php
+++ rot13.php
@@ -1,5 +1,9 @@
 <?php
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 socker_bind($sock, '0.0.0.0', 9001);
-
+for (;;) {
+  socket_recvfrom($sock, $message, 1024, 0, $ip, $port);
+  $reply = str_rot13($message);
+  socket_sendto($sock, $reply, strlen($reply), 0, $ip, $port);
+}
 
